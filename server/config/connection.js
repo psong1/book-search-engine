21fa4473ac6890c5de669a2fda70ae21be01ecfb
psong1 <paulsong29@gmail.com>
2023-05-31 20:00:06 -0400

    added OR operator

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI,
-  "mongodb://127.0.0.1:27017/googlebooks",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
