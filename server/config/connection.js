43bfa876c9fd54e798e8f136ab43aba07fe60b10
psong1 <paulsong29@gmail.com>
2023-06-08 19:33:00 -0400

    working on connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
+  `mongodb+srv://psong1:${process.env.DB_PW}@cluster0.gkd8vjw.mongodb.net/googlebooks?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
